a8cce6675d67013a613ef61ccaed83bd044bf81e
mohammed saad <madanapallishaikmohammedsaad@gmail.com>
2026-09-14 22:32:59 +0530

    Improve responsive app layout spacing

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,13 +4,15 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0">
-        <Navbar />
-        <main className="flex-1 p-8 overflow-x-hidden">
-          <Outlet />
-        </main>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <div className="flex min-w-0 flex-1 flex-col">
+          <Navbar />
+          <main className="min-w-0 flex-1 overflow-x-hidden p-4 sm:p-6 lg:p-8">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   );
